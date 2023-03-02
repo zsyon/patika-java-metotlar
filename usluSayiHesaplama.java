9f55cf464c7b87b3d31d6ae30069aebe3923ae94
HP <20060339@stu.omu.edu.tr>
2023-03-02 22:03:00 +0300

    add for

--- usluSayiHesaplama.java
+++ usluSayiHesaplama.java
@@ -1,6 +1,21 @@
 import java.util.Scanner;
 
 public class usluSayiHesaplama {
+
+  static int us(int taban, int us){
+
+    for(int i=us; i<1 ; i++){
+
+      
+
+    }
+
+
+
+    return 1;
+
+  }
+
   public static void main(String[] args) {
 
     Scanner input = new Scanner(System.in);
