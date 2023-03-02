bbccbfd2d86374d6cbedae828b0c8cc592704e6e
HP <20060339@stu.omu.edu.tr>
2023-03-02 21:53:24 +0300

    get value from user

--- usluSayiHesaplama.java
+++ usluSayiHesaplama.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class usluSayiHesaplama {
   public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+
+    int taban, us;
+
+    System.out.print("Taban değeri giriniz : ");
+    taban = input.nextInt();
+
+    System.out.print("Üs değeri giriniz : ");
+    us = input.nextInt();
+
+
+
     
   }
   
